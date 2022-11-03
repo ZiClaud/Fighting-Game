a110c1fa7f9624089eb1f305b2105b8bdf8826f8
Claudio Di Maio <claudiodimaio01@gmail.com>
2022-11-03 11:00:51 +0100

    Improved code

---
+++ src/main/java/actions/ActionType.java
@@ -0,0 +1,7 @@
+package actions;
+
+public enum ActionType {
+    Fight(),
+    Facing(),
+    Jump()
+}
--- src/main/java/actions/fight/Fight.java
+++ src/main/java/actions/fight/Fight.java
@@ -1,6 +1,11 @@
 package actions.fight;
 
 import actions.Action;
+import actions.ActionType;
+import characters.Player;
 
 public class Fight extends Action {
+    public Fight(Player player, ActionType actionType) {
+        super(player, actionType);
+    }
 }
