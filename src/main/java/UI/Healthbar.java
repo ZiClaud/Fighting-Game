4e3de46afef3debb17b74fe275692df48e705145
Claudio Di Maio <claudiodimaio01@gmail.com>
2022-11-10 09:21:00 +0100

    Now player takes damage only if there's collision between him and the enemy

--- src/main/java/UI/Healthbar.java
+++ src/main/java/UI/Healthbar.java
@@ -17,42 +17,52 @@ public class Healthbar extends GameObject {
         this.handler = handler;
     }
 
+    private void collision(Player player, Player enemy) {   // TODO: Put this somewhere else and make it public
+        /*
+        System.out.println("Player X: " + ((Player) player).getSize().getActualX(player.getX()) + ", " + ((Player) player).getSize().getActualRightX(player.getX()));
+        System.out.println("Enemy X: " + ((Player) enemy).getSize().getActualX(enemy.getX()) + ", " + ((Player) enemy).getSize().getActualRightX(enemy.getX()));
+        System.out.println("True/False: " + (((Player) player).getSize().getActualX(player.getX()) >= ((Player) enemy).getSize().getActualX(enemy.getX())) + ", " + (((Player) player).getSize().getActualRightX(player.getX()) <= ((Player) enemy).getSize().getActualRightX(enemy.getX())));
+        */
+
+        /// Check if they have the same X
+        if (player.getSize().getActualX(player.getX()) <= enemy.getSize().getActualRightX(enemy.getX())
+                && player.getSize().getActualRightX(player.getX()) >= enemy.getSize().getActualX(enemy.getX())) {
+            /// Check if they have the same Y
+            if (player.getSize().getActualY(player.getY()) <= enemy.getSize().getActualBottomY(enemy.getY())
+                    && player.getSize().getActualBottomY(player.getY()) >= enemy.getSize().getActualY(enemy.getY())) {
+
+                /**
+                 * Hit taken
+                 */
+                player.getAction().setActionType(PlayerAnimationType.TakeHit);
+
+                /**
+                 * Death
+                 */
+                if (player.getHp() < 0) {
+                    player.getAction().setActionType(PlayerAnimationType.Death);
+
+                    System.out.println("Enemy won!"); // TODO: Print this in game
+
+                    player.setX(50);
+                    enemy.setX(Game.WIDTH - 50 - 163);
+                    // TODO: Add countdown
+
+
+                    // TODO: Reset HPs - FIX
+                    player.setHp(player.getHp() + 150);
+                }
+                player.setHp(player.getHp() - 1);
+            }
+        }
+    }
+
     @Override
     public void tick() {
         for (GameObject player : handler.objects) {
             for (GameObject enemy : handler.objects) {
                 if (player.getId() == ID.Player && enemy.getId() == ID.Enemy) {
-                    /*
-                    System.out.println("Player X: " + ((Player) player).getSize().getActualX(player.getX()) + ", " + ((Player) player).getSize().getActualRightX(player.getX()));
-                    System.out.println("Enemy X: " + ((Player) enemy).getSize().getActualX(enemy.getX()) + ", " + ((Player) enemy).getSize().getActualRightX(enemy.getX()));
-                    System.out.println("True/False: " + (((Player) player).getSize().getActualX(player.getX()) >= ((Player) enemy).getSize().getActualX(enemy.getX())) + ", " + (((Player) player).getSize().getActualRightX(player.getX()) <= ((Player) enemy).getSize().getActualRightX(enemy.getX())));
-                     */
-                    if (((Player) player).getSize().getActualX(player.getX()) <= ((Player) enemy).getSize().getActualRightX(enemy.getX())
-                            && ((Player) player).getSize().getActualRightX(player.getX()) >= ((Player) enemy).getSize().getActualX(enemy.getX())
-                    ) {
-                        /**
-                         * Hit taken
-                         */
-                        ((Player) player).getAction().setActionType(PlayerAnimationType.TakeHit);
-
-                        /**
-                         * Death
-                         */
-                        if (((Player) player).getHp() < 0) {
-                            ((Player) player).getAction().setActionType(PlayerAnimationType.Death);
-
-                            System.out.println("Enemy won!"); // TODO: Print this in game
-
-                            player.setX(50);
-                            enemy.setX(Game.WIDTH - 50 - 163);
-                            // TODO: Add countdown
-
-
-                            // TODO: Reset HPs - FIX
-                            ((Player) player).setHp(((Player) player).getHp() + 150);
-                        }
-                        ((Player) player).setHp(((Player) player).getHp() - 1);
-                    }
+                    collision((Player) player, (Player) enemy);
                 }
             }
 
