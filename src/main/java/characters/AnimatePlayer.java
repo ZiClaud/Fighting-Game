90ceb51acc7f1afc4a4bb7904d8d55cd17d404c0
Claudio Di Maio <claudiodimaio01@gmail.com>
2022-11-15 16:38:04 +0100

    Re-added "event" in AnimatePlayer

--- src/main/java/characters/AnimatePlayer.java
+++ src/main/java/characters/AnimatePlayer.java
@@ -34,12 +34,26 @@ public class AnimatePlayer implements ObserverAnimation {
             //System.out.println("I: " + i);
             player.getPlayerImage().setImg(currentAnimation.get(i));
             i++;
+            event();
         });
         timer.setRepeats(true);
         timer.setCoalesce(true);
         timer.start();
     }
 
+    private void event(){   // TODO: Use wisely
+        if (player.getId() == ID.Player) {
+            if (action.getActionType() == PlayerAction.Attack1 && i == 5) {
+                System.out.println("Attack1! by player");
+//            Events.hit();
+            }
+        } else if (player.getId() == ID.Enemy) {
+            if (action.getActionType() == PlayerAction.Attack1 && i == 3) {
+                System.out.println("Attack1! by enemy");
+//            Events.hit();
+            }
+        }
+    }
     @Override
     public void update(ActionInterface action) {
         this.action = action;
