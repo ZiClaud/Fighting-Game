6b70fed19bf87821e63eeaf9120f49a140aee96d
Claudio Di Maio <claudiodimaio01@gmail.com>
2022-11-26 14:27:27 +0100

    Improved Menu

--- src/main/java/window/UI/MenuWindow.java
+++ src/main/java/window/UI/MenuWindow.java
@@ -1,20 +1,39 @@
 package window.UI;
 
+import utils.MyUtils;
 import window.game.Game;
 import window.game.KeyInput;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class MenuWindow extends JFrame {
     JFrame frame;
+
     public MenuWindow() {
         Theme.setup();
         Theme.theme();
 
         JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(2, 4));
+        try {
+            BufferedImage myPicture1 = ImageIO.read(new File("src/main/resources/Spirit/Idle/Idle_1.png"));
+            BufferedImage myPicture2 = ImageIO.read(new File("src/main/resources/Undead/Idle/Idle_1.png"));
+            myPicture2 = MyUtils.mirrorImage(myPicture2);
+            mainPanel.add(new JLabel(new ImageIcon(myPicture1)));
+            mainPanel.add(new JPanel());
+            mainPanel.add(new JPanel());
+            mainPanel.add(new JLabel(new ImageIcon(myPicture2)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         JButton btPVP = new JButton("PvP");
         JButton btEasy = new JButton("Easy");
         JButton btMedium = new JButton("Medium");
