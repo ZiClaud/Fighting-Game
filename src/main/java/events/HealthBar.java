6d027dcfb645b97713ff1bf131e5c6a078f15ff9
Claudio Di Maio <claudiodimaio01@gmail.com>
2022-12-02 14:19:57 +0100

    Improved HealthBar

--- src/main/java/events/HealthBar.java
+++ src/main/java/events/HealthBar.java
@@ -8,10 +8,12 @@ import java.awt.*;
 
 public class HealthBar extends GameObject {
     CharacterInt player;
+    private final int initialHp;
 
     public HealthBar(int x, int y, ID id, CharacterInt player) {
         super(x, y, id);
         this.player = player;
+        this.initialHp = player.getHp();
     }
 
     @Override
@@ -24,9 +26,15 @@ public class HealthBar extends GameObject {
         if (id == ID.HealthBarPlayer) {
             g.setColor(Color.GREEN);
             g.fillRect(x, y, player.getHp() * 3 / 2, 13 * 3 / 2);
+            g.setColor(Color.BLACK);
+            g.drawRect(x, y, initialHp * 3 / 2, 13 * 3 / 2);
+            g.drawRect(x - 1, y - 1, initialHp * 3 / 2 + 2, 13 * 3 / 2 + 2);
         } else if (id == ID.HealthBarEnemy) {
             g.setColor(Color.RED);
             g.fillRect(x - player.getHp() * 3 / 2, y, player.getHp() * 3 / 2, 13 * 3 / 2);
+            g.setColor(Color.BLACK);
+            g.drawRect(x - initialHp * 3 / 2, y, initialHp * 3 / 2, 13 * 3 / 2);
+            g.drawRect(x - initialHp * 3 / 2 - 1, y - 1, initialHp * 3 / 2 + 2, 13 * 3 / 2 + 2);
         }
     }
 }
